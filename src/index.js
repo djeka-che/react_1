6f44d742af1923f61d24b13100f82c5db2cf6137
djeka-che@mail.ru <djeka-che@mail.ru>
2021-05-02 15:55:59 +0300

    fin1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,89 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom";
+import './App.css';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <>
+        <header className={'header'}>
+            <ul className='Nav'>
+                <li>
+                    <a href={'#'}>
+                        HTML
+                    </a>
+                </li>
+                <li>
+                    <a href={'#'}>
+                        JS
+                    </a>
+                </li>
+                <li>
+                    <a href={'#'}>
+                        CSS
+                    </a>
+                </li>
+                <li>
+                    <a href={'#'}>
+                        REACT
+                    </a>
+                </li>
+            </ul>
+        </header>
+        <main>
+            <div>
+                <table>
+                    <tr>
+                        <td>
+                            <img src='HTML.png' alt={'Картинка HTML'} width={'560px'} height={'300px'}/>
+                        </td>
+                        <td>
+                            <Lorem/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <img src='JS.jpg' alt={'Картинка JS'} width={'560px'} height={'300px'}/>
+                        </td>
+                        <td>
+                            <Lorem/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <img src='css.png' alt={'Картинка css'} width={'560px'} height={'300px'}/>
+                        </td>
+                        <td>
+                            <Lorem/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <img src='react.png' alt={'Картинка react'} width={'560px'} height={'300px'}/>
+                        </td>
+                        <td>
+                            <Lorem/>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </main>
+        <footer className={'header'}>
+            <div className={'footer-img'}>
+                <Img/><Img/><Img/><Img/><Img/><Img/><Img/><Img/><Img/><Img/><Img/><Img/><Img/><Img/><Img/><Img/><Img/>
+                <Img/><Img/><Img/><Img/><Img/>
+            </div>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+        </footer>
+    </>,
+    document.getElementById('root')
+);
+    function Lorem() {
+        return "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt " +
+        "ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco " +
+        "laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in " +
+        "voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat " +
+        "cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    }
+    function Img() {
+        return <img src='IT.jpg' alt={'Картинка react'} width={'50px'} height={'50px'}
+        />
+    }
